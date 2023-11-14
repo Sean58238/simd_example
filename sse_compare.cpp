401d14df6bdc4bcb447d18ca7182ede8a5374b42
Sean58238 <xiang.lin@intel.com>
2023-11-14 11:35:39 +0800

    add sse compare

---
+++ sse_compare.cpp
@@ -0,0 +1,85 @@
+/*
+sse4.2 compare demo
+g++ --std=c++14 -O2 -msse sse_compare.cpp -o sse_compare
+Lin,Xiang
+*/
+
+#include <iostream>
+#include <immintrin.h>
+#include <nmmintrin.h>
+#include <sys/time.h>
+
+#define N 4*100000
+
+ float op1[N];
+ float op2[N];
+ float result1[N];
+ float result2[N];
+
+
+
+void init()
+{
+	for(int i = 0;i < N; i++)
+	{
+		op1[i] = (float)rand()/(float)RAND_MAX;
+		op2[i] = (float)rand()/(float)RAND_MAX;
+	}
+}
+
+
+void add()
+{
+	#pragma loop(no_vector)
+	for(int i = 0; i < N;i++){
+		result1[i] = op1[i] + op2[i];
+	}
+}
+ 
+void add_sse()
+{
+	__m128  a;
+	__m128  b;
+	__m128  c;
+ 
+	for(int i = 0; i < N;i = i + 4)
+	{
+		// Load
+		a = _mm_load_ps(op1 + i);
+		b = _mm_load_ps(op2 + i);
+ 
+		c = _mm_add_ps(a, b);	// c = a + b
+ 
+		_mm_store_ps(result2 + i, c);
+	}
+}
+
+
+int main(int argc, char* argv[])
+{
+	init();
+	//srand((unsigned int)time(NULL));
+ 
+    struct timeval t1,t2;
+    double timeuse;
+    printf("Add a vector\n");
+
+    gettimeofday(&t1,NULL);
+	add();
+	gettimeofday(&t2,NULL);
+    timeuse = (t2.tv_sec - t1.tv_sec) + (double)(t2.tv_usec - t1.tv_usec)/1000000000.0;
+    std::cout<<"Elapsed time (ms) : "<<timeuse<<std::endl;  //输出时间（单位：ms）
+
+
+    struct timeval t3,t4;
+	double timeuse2;
+	printf("\n");
+	printf("Add a vector with SSE4.2 instructions\n");
+	gettimeofday(&t3,NULL);
+	add_sse();
+	gettimeofday(&t4,NULL);
+    timeuse2 = (t4.tv_sec - t3.tv_sec) + (double)(t4.tv_usec - t3.tv_usec)/1000000000.0;
+    std::cout<<"Elapsed time (ms): "<<timeuse2<<std::endl;  //输出时间（单位：ms）
+ 
+	return 0;
+}
